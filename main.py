eb7fe4cfa3169f192e59ce57c198b6527f245497
JhonAndN <backtosecondend@gmail.com>
2024-03-25 16:39:25 +0200

    Finished Project

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
-path_to_file = 'C:/Users/dzoni/workspace/github.com/bookbot/bookbot/books/frankenstein.txt'
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-def Print(Text):
-    print(Text)
+def sort_on(d):
+    return d["num"]
 
-def ReadFile():
-    with open(path_to_file) as f:
-        file_contents = f.read()
-    return file_contents
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
-def CountWords(Text):
-    print(len(Text.split()))
-
-def Count_Letters(Text):
-    Counted = {}
-    for c in Text:
-        if c.lower() in Counted:
-            Counted[c.lower()] += 1
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
         else:
-            Counted[c.lower()] = 1
-    print(Counted)
-       
+            chars[lowered] = 1
+    return chars
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
 def main():
-    AllText = ReadFile()
-    CountWords(AllText)
-    Count_Letters(AllText)
+    Path_To_book = "books/frankenstein.txt"
+    text = get_book_text(Path_To_book)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-if __name__ == '__main__':
-    main()
+    print(f"--- Begin report of {Path_To_book} ---")
+    print(f"{num_words} words found in the document")
+    print()
 
-#"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    print("--- End report ---")
+
+main()
